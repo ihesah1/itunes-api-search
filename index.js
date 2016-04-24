d292bb22bc81fd269306805935c9f9fe237eba6b
Eduardo Menoncello <eduardomenoncello@MacBook-Pro-de-Eduardo.local>
2016-04-24 11:41:44 -0300

    - Fixed to JSLint - Change to named function

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 'use strict';
-let querystring = require('querystring')
+
+let querystring = require('querystring');
 let request = require('request');
 let S = require('string');
 
 exports.search = search;
 
-const baseUrl = "http://itunes.apple.com/search?";
+const baseUrl = 'http://itunes.apple.com/search?';
 
 function search(term, options, callback) {
   if (!term) {
-    callback({ text: 'search term is required'})
+    callback({ text: 'search term is required' });
     return;
   }
 
@@ -19,18 +20,20 @@ function search(term, options, callback) {
 
   let url = baseUrl + querystring.stringify(options);
 
-  request(url, function(err, header, res) {
+  request(url, requestCallback);
+
+  function requestCallback(err, header, res) {
     if (err) {
       callback(err);
       return;
     }
 
     try {
-      var data = JSON.parse(res)
+      var data = JSON.parse(res);
       callback(null, data);
     } catch (e) {
       callback({ text: S(S(res).stripTags().s).replaceAll('  ', ' ').s.substring(5) });
       return;
     }
-  })
+  }
 }
